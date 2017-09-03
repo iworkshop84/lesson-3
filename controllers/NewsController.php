05ffd1b10dbeeb0618963d6ffefa9d984c69a276
iworkshop <iworkshop84@gmail.com>
2017-09-03 23:24:32 +0300

    Автозагрузка

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../models/News.php';
+
 
 class NewsController
 {
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 //require __DIR__ . '/controllers/NewsController.php';
 
+require_once __DIR__ . '/autoload.php';
+
 $ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
 $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
@@ -8,7 +10,7 @@ $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 //$controller->actionAll();
 
 $controllerClassName = $ctrl . 'Controller';
-require_once __DIR__ . '/controllers/' . $controllerClassName . '.php';
+
 
 $controller = new $controllerClassName;
 $method = 'action' . $act;
