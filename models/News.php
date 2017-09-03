95d376c1ed9719e47fd5d9def8775cc20c627b11
iworkshop <iworkshop84@gmail.com>
2017-09-03 23:48:19 +0300

    Позднее связывание

--- models/News.php
+++ models/News.php
@@ -2,22 +2,17 @@
 
 
 class News
+    extends AbstractModel
 {
     public $news_id;
     public $news_name;
     public $news_content;
     public $news_date;
 
-    public static function getAll()
-    {
-        $db = new DB();
-        return $db->queryAll('SELECT * FROM news', 'News');
-    }
+    protected static $table = 'news';
+    protected static $class = 'News';
 
-    public static function getOne($id)
-    {
-        $db = new DB();
-        return $db->queryAll("SELECT * FROM news WHERE news_id=" . $id, 'News');
-    }
+
+
 
 }
